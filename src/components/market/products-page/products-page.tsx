0adfda4512d0afecec081c88a7d25786a2a25c08
Eddie C. <edimc2000@gmail.com>
2026-02-04 09:37:55 -0600

    preparation to break down products page

--- src/components/market/products-page/products-page.tsx
+++ src/components/market/products-page/products-page.tsx
@@ -1,14 +1,14 @@
-// import type { JSX } from "react"
+import type { JSX } from "react"
 import { CATEGORIES, type ICategory } from "../models/category"
 
 import './products-page.css'
 
-const categoryComponents = CATEGORIES.map((category) => {
+const categoryComponents: JSX.Element[] = CATEGORIES.map((category: ICategory) => {
 
     console.log(` category: ${category} `)
 
     return (
-        <div className="product-item">
+        <div className="product-item" key={category.name}>
             <span className="category-title">{category.name}</span>
             <img className="category-image" src={category.imageUrl} alt={category.description + ' image'} />
             <span className="category-longdesc">{category.longDescription}</span>
