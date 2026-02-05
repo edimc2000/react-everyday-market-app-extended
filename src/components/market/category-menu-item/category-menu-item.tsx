f100cb448481cb550da46ec5cb14417a2d0cf33b
Eddie C. <edimc2000@gmail.com>
2026-02-04 19:18:41 -0600

    tried out button click react way

--- src/components/market/category-menu-item/category-menu-item.tsx
+++ src/components/market/category-menu-item/category-menu-item.tsx
@@ -7,10 +7,27 @@ interface CategoryItemProps {
     description: string
 }
 
-const CategoryItem = (props: CategoryItemProps): JSX.Element => {
-    const { name, imageUrl, longDescription, description} = props
+
+function OnCatSelected(props: CategoryItemProps): JSX.Element {
+    const { name } = props
+    
+    function handleClick(categoryName: string) {
+        console.log(`button clicked: ${categoryName}`)
+    }
+
     return (
         <>
+            <button onClick={() => handleClick(name)}> TESTING THIS BUTTON</button>
+        </>
+    )
+}
+
+const CategoryItem = (props: CategoryItemProps): JSX.Element => {
+    const { name, imageUrl, longDescription, description } = props
+    return (
+        <>
+            <OnCatSelected {...props} />
+            testing
             <span className="category-title">{name}</span>
             <img className="category-image" src={imageUrl} alt={description + ' image'} />
             <span className="category-longdesc">{longDescription}</span>
